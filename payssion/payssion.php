86b6fdf413731e5916537ae9ab1167b23f781065
payssion <github@payssion.com>
2015-11-24 23:51:12 +0800

    added rixty

--- payssion/payssion.php
+++ payssion/payssion.php
@@ -44,10 +44,10 @@ class Payssion extends PaymentModule
 				OR !Configuration::updateValue(self::PAYSSION_API_KEY, '')
 				OR !Configuration::updateValue(self::PAYSSION_SECRET_KEY, '')
 				OR !Configuration::updateValue(self::PAYSSION_PM_OPTIONS, 
-						'bitcoin|cashu|onecard|paysafecard|sofort|qiwi|neosurf|trustpay|dineromail_ar|boleto_br|bancodobrasil_br|bradesco_br|caixa_br|itau_br|elo_br|hipercard_br|visa_br|mastercard_br|dinersclub_br|americanexpress_br|banamex_mx|bancomer_mx|oxx_mx|santander_mx|redpagos_uy|bancochile_cl|redcompra_cl|molpay|maybank2u_my|dragonpay_ph')
+						'bitcoin|cashu|onecard|rixty|paysafecard|sofort|qiwi|neosurf|trustpay|dineromail_ar|boleto_br|bancodobrasil_br|bradesco_br|caixa_br|itau_br|elo_br|hipercard_br|visa_br|mastercard_br|dinersclub_br|americanexpress_br|banamex_mx|bancomer_mx|oxx_mx|santander_mx|redpagos_uy|bancochile_cl|redcompra_cl|molpay|maybank2u_my|dragonpay_ph')
 				OR !Configuration::updateValue(self::PAYSSION_PM_SURCHARGE, '0|0|0|0|0|0')
 				OR !Configuration::updateValue(self::PAYSSION_PM_NAME, 
-						'Bitcoin|CashU|OneCard|Paysafecard|SOFORT|Qiwi|Neosurf|trustpay|Dinero Mail - Efectivo Argentinal|Boleto|Banco do Brasil|Bradesco|Caixa Brazil|Itau|Elo Brazil|Hipercard Brazil|Visa Brazil|Mastercard Brazil|Dinersclub Brazil|American Express Brazil|Banamex|Bancomer(BBVA)|Oxxo|Santander Mexico|Redpagos|Banco de Chile|RedCompra|MOLPay|Maybank2u|Dragonpay')
+						'Bitcoin|CashU|OneCard|Rixty|Paysafecard|SOFORT|Qiwi|Neosurf|trustpay|Dinero Mail - Efectivo Argentinal|Boleto|Banco do Brasil|Bradesco|Caixa Brazil|Itau|Elo Brazil|Hipercard Brazil|Visa Brazil|Mastercard Brazil|Dinersclub Brazil|American Express Brazil|Banamex|Bancomer(BBVA)|Oxxo|Santander Mexico|Redpagos|Banco de Chile|RedCompra|MOLPay|Maybank2u|Dragonpay')
 				OR !Configuration::updateValue(self::PAYSSION_PM_ENABLED, '')
 				OR !$this->registerHook('payment')
 				OR !$this->registerHook('paymentReturn'))
